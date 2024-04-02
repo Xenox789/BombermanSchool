62ea3cdba1daaa4e6488037f3e807c3a6da7ae51
delg87 <delg87@inf.elte.hu>
2024-04-02 18:00:21 +0200

    some changes in levelgenerator

--- Bomberman/Assets/Model/Scripts/LevelGenerator.cs
+++ Bomberman/Assets/Model/Scripts/LevelGenerator.cs
@@ -17,8 +17,8 @@ public class LevelGenerator : MonoBehaviour
 
     void Start()
     {
-        // GenerateLevel();
-        LoadLevel();
+         GenerateLevel();
+        //LoadLevel();
 
         // SaveLevel();
     }
