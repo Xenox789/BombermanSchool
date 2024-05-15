ad02f2e7dd23d857dc1287f7cb2b07cfa6d883aa
Máté Takács <wlxjh6@INF.ELTE.HU>
2024-05-15 23:03:54 +0200

    BombcontrollerTest

--- Assets/Model/Scripts/BombController.cs
+++ Assets/Model/Scripts/BombController.cs
@@ -153,10 +153,10 @@ public class BombController : MonoBehaviour
         bombsRemaining++;
     }
 
-    private void IsAnyBombRemaining()
+    public void IsAnyBombRemaining()
     {
         if (bombsRemaining == 0) _IsAnyBombRemaining = true;
-        _IsAnyBombRemaining = false;
+        else _IsAnyBombRemaining = false;
     }
 
 
